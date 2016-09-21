b567eb141c25580d8df77629ac1691c5981c9cae
shaunwbell <shaun.bell@noaa.gov>
2016-09-21 14:30:03 -0700

    add ability to plot geojson features on plot

--- ArcticHeatAirCraftCSV2Map.py
+++ ArcticHeatAirCraftCSV2Map.py
@@ -10,6 +10,7 @@ Takes parsed aircraft data and plots it
 import datetime
 import pandas as pd
 import argparse
+import pygeoj
 
 #Science Stack
 import numpy as np
@@ -18,9 +19,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from matplotlib.mlab import bivariate_normal
+import matplotlib.ticker as ticker
 import cartopy.feature as cfeature
 import cartopy.crs as ccrs
-
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 
 __author__   = 'Shaun Bell'
 __email__    = 'shaun.bell@noaa.gov'
@@ -52,11 +54,14 @@ parser = argparse.ArgumentParser(description='Map')
 parser.add_argument('sourcedir', metavar='sourcedir', type=str, help='full path to file')
 parser.add_argument('mapdir', metavar='mapdir', type=str, help='full path to file')
 parser.add_argument('maxalt', metavar='maxalt', type=float, help='altitude above which data is ignored')
+parser.add_argument('--stations', type=str, help='optional full path to geojson file')
 args = parser.parse_args()
 
 ## read data in
 data = pd.read_csv(args.sourcedir,names=['lat','lon','alt','sst','pyro'],header=0)
 
+if args.stations:
+    geo_data = pygeoj.load(filepath=args.stations)
 
 range_int = 1.
 extent = [data['lat'].min()-range_int, data['lat'].max()+range_int, \
@@ -68,7 +73,7 @@ if maptype == 'cartopy':
     
     fig = plt.figure()
     plt.subplot(1,1,1)
-    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax = plt.axes(projection=ccrs.Mercator())
     ax.set_extent(extent)
 
     
@@ -85,6 +90,21 @@ if maptype == 'cartopy':
         cmap='seismic', transform=ccrs.PlateCarree(), zorder=2)
     c = plt.colorbar(extend='both', shrink=0.50)
 
+    if args.stations:
+        for feature in geodata:
+            (glat,glon) = feature.geometry.coordinates
+            plt.scatter(glat,glon,10,
+                marker='+', edgecolors='none', color='black', 
+                transform=ccrs.PlateCarree(),zorder=3)
     #plt.show()
+    g1 = ax.gridlines(crs=ccrs.PlateCarree(),
+                  linewidth=.25, color='gray', alpha=0.5, linestyle='--')
+    g1.xlocator = ticker.FixedLocator([-156, -160, -164, -168, -172, -176, 180])
+    g1.ylocator = ticker.FixedLocator([64, 66, 68, 70, 72, 74, 76])
+    g1.xformatter = LONGITUDE_FORMATTER
+    g1.yformatter = LATITUDE_FORMATTER
+    g1.ylabels_left = True
+    g1.xlabels_top = True
+
     plt.savefig('ArcticHeat_SST.png', dpi = (300))
     plt.close()
