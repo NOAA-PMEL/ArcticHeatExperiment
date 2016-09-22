ccf00adbfa8453457a5ba8219a2c978452b717e9
shaunwbell <shaun.bell@noaa.gov>
2016-09-22 15:24:04 -0700

    ctd sparklines

---
+++ ArcticHeatCTDsparklines.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+"""
+ArcticHeatCTDsparklines.py
+
+Create miniature sparkline CTD plots for XBT/AXCTD data
+
+"""
+
+#System Stack
+import datetime
+import argparse
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+__author__   = 'Shaun Bell'
+__email__    = 'shaun.bell@noaa.gov'
+__created__  = datetime.datetime(2016, 9, 22)
+__modified__ = datetime.datetime(2016, 9, 22)
+__version__  = "0.1.0"
+__status__   = "Development"
+__keywords__ = 'arctic heat','ctd','FOCI', 'wood', 'kevin'
+
+mpl.rcParams['axes.grid'] = False
+mpl.rcParams['axes.edgecolor'] = 'white'
+mpl.rcParams['axes.linewidth'] = 0.25
+mpl.rcParams['grid.linestyle'] = '--'
+mpl.rcParams['grid.linestyle'] = '--'
+mpl.rcParams['xtick.major.size'] = 2
+mpl.rcParams['xtick.minor.size'] = 1
+mpl.rcParams['xtick.major.width'] = 0.25
+mpl.rcParams['xtick.minor.width'] = 0.25
+mpl.rcParams['ytick.major.size'] = 2
+mpl.rcParams['ytick.minor.size'] = 1
+mpl.rcParams['xtick.major.width'] = 0.25
+mpl.rcParams['xtick.minor.width'] = 0.25
+mpl.rcParams['ytick.direction'] = 'out'
+mpl.rcParams['xtick.direction'] = 'out'
+mpl.rcParams['ytick.color'] = 'grey'
+mpl.rcParams['xtick.color'] = 'grey'
+# Example of making your own norm.  Also see matplotlib.colors.
+# From Joe Kington: This one gives two different linear ramps:
+
+class MidpointNormalize(colors.Normalize):
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
+
+"""----------------------------- Main -------------------------------------"""
+
+parser = argparse.ArgumentParser(description='ArcticHeat ctd datafile parser ')
+parser.add_argument('filepath', metavar='filepath', type=str,
+               help='full path to file')
+parser.add_argument('-axctd','--axctd', action="store_true",
+	help='work with axctd data')
+parser.add_argument('-xbt','--xbt', action="store_true",
+	help='work with xbt data')
+parser.add_argument('--maxdepth', type=float, help="known bathymetric depth at location")
+parser.add_argument('--paramspan', nargs='+', type=float, help="max,min of parameter")
+
+args = parser.parse_args()
+
+if args.xbt:
+	xbtdata = pd.read_csv(args.filepath, delim_whitespace=True, skiprows=4, na_values='******')
+
+	if args.maxdepth:
+		figscale = args.maxdepth / 15. #makes most of the chukchi a 1x3 inch image
+	else:
+		figscale = 3.
+
+	fig = plt.figure(1, figsize=(1, 3), facecolor='w', edgecolor='w')
+	ax1 = fig.add_subplot(111)
+	#p1 = ax1.plot(xbtdata['(C)'],xbtdata['Depth'],'r')
+	p1 = ax1.scatter(xbtdata['(C)'],xbtdata['Depth'],5,marker='.', edgecolors='none', c=xbtdata['(C)'], 
+        norm=MidpointNormalize(midpoint=0.),
+        vmin=args.paramspan[0], vmax=args.paramspan[1], 
+        cmap='seismic')
+
+	p1 = ax1.plot(np.zeros_like(xbtdata['Depth']),xbtdata['Depth'],'grey',linewidth=.1)
+	ax1.set_yticks(np.arange(0.,args.maxdepth + 25.,10.))
+	if args.maxdepth:
+		ax1.set_ylim([0,args.maxdepth])
+
+	if args.paramspan:
+		ax1.set_xlim([args.paramspan[0],args.paramspan[1]])
+
+	ax1.invert_yaxis()
+
+	fmt=mpl.ticker.ScalarFormatter(useOffset=False)
+	fmt.set_scientific(False)
+	ax1.xaxis.set_major_formatter(fmt)
+	ax1.tick_params(axis='both', which='major')
+	ax1.xaxis.set_ticklabels([])
+	ax1.yaxis.set_ticklabels([])
+	plt.tight_layout()
+	plt.savefig(args.filepath.split('.')[0] + '.png', transparent=True, dpi = (150))
+	plt.close()
---
+++ scripts/sparklines.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+#9/10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160910_data/AH20160910_ax/log00000.dta -xbt --maxdepth 153 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160910_data/AH20160910_ax/log00001.dta -xbt --maxdepth 2010 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160910_data/AH20160910_ax/log00002.dta -xbt --maxdepth 78 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160910_data/AH20160910_ax/log00003.dta -xbt --maxdepth 760 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160910_data/AH20160910_ax/log00004.dta -xbt --maxdepth 1976 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160910_data/AH20160910_ax/log00005.dta -xbt --maxdepth 150 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160910_data/AH20160910_ax/log00006.dta -xbt --maxdepth 68 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160910_data/AH20160910_ax/log00007.dta -xbt --maxdepth 59 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160910_data/AH20160910_ax/log00008.dta -xbt --maxdepth 47 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160910_data/AH20160910_ax/log00009.dta -xbt --maxdepth 60 --paramspan -5 10
+
+#9/11
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160911_data/AH20160911_ax/log00000.dta -xbt --maxdepth 30 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160911_data/AH20160911_ax/log00001.dta -xbt --maxdepth 39 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160911_data/AH20160911_ax/log00002.dta -xbt --maxdepth 41 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160911_data/AH20160911_ax/log00003.dta -xbt --maxdepth 40 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160911_data/AH20160911_ax/log00004.dta -xbt --maxdepth 46 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160911_data/AH20160911_ax/log00005.dta -xbt --maxdepth 55 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160911_data/AH20160911_ax/log00006.dta -xbt --maxdepth 47 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160911_data/AH20160911_ax/log00007.dta -xbt --maxdepth 38 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160911_data/AH20160911_ax/log00008.dta -xbt --maxdepth 39 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160911_data/AH20160911_ax/log00009.dta -xbt --maxdepth 40 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160911_data/AH20160911_ax/log00010.dta -xbt --maxdepth 39 --paramspan -5 10
+
+#9/12
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160912_data/AH20160912_ax/log00011.dta -xbt --maxdepth 53 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160912_data/AH20160912_ax/log00012.dta -xbt --maxdepth 48 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160912_data/AH20160912_ax/log00013.dta -xbt --maxdepth 40 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160912_data/AH20160912_ax/log00014.dta -xbt --maxdepth 30 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160912_data/AH20160912_ax/log00015.dta -xbt --maxdepth 30 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160912_data/AH20160912_ax/log00016.dta -xbt --maxdepth 30 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160912_data/AH20160912_ax/log00017.dta -xbt --maxdepth 26 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160912_data/AH20160912_ax/log00018.dta -xbt --maxdepth 20 --paramspan -5 10
+
+
+#9/14
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160914_data/AH20160914_ax/log00000.dta -xbt --maxdepth=51 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160914_data/AH20160914_ax/log00001.dta -xbt --maxdepth=50 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160914_data/AH20160914_ax/log00002.dta -xbt --maxdepth=47 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160914_data/AH20160914_ax/log00003.dta -xbt --maxdepth=43 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160914_data/AH20160914_ax/log00004.dta -xbt --maxdepth=50 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160914_data/AH20160914_ax/log00005.dta -xbt --maxdepth=42 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160914_data/AH20160914_ax/log00006.dta -xbt --maxdepth=43 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160914_data/AH20160914_ax/log00007.dta -xbt --maxdepth=33 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160914_data/AH20160914_ax/log00008.dta -xbt --maxdepth=23 --paramspan -5 10
+
+#9/15
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160915_data/AH20160915_ax/log00005.dta -xbt --maxdepth=42 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160915_data/AH20160915_ax/log00004.dta -xbt --maxdepth=39 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160915_data/AH20160915_ax/log00003.dta -xbt --maxdepth=40 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160915_data/AH20160915_ax/log00002.dta -xbt --maxdepth=38 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160915_data/AH20160915_ax/log00001.dta -xbt --maxdepth=43 --paramspan -5 10
+python ArcticHeatCTDsparklines.py /Volumes/WDC_internal/Users/bell/in_and_outbox/2016/wood/ArcticHeat/sep/AH20160915_data/AH20160915_ax/log00000.dta -xbt --maxdepth=33 --paramspan -5 10
