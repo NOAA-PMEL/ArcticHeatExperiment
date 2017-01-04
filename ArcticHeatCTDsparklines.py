49ecbdca34f3cf2c944b0d03f9b925f5c4499a90
shaunwbell <shaun.bell@noaa.gov>
2017-01-04 14:38:00 -0800

    update to plot routines

--- ArcticHeatCTDsparklines.py
+++ ArcticHeatCTDsparklines.py
@@ -25,6 +25,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
+from io_utils.EcoFOCI_db_io import EcoFOCI_db_ALAMO
+
 __author__   = 'Shaun Bell'
 __email__    = 'shaun.bell@noaa.gov'
 __created__  = datetime.datetime(2016, 9, 22)
@@ -79,7 +81,12 @@ parser.add_argument('--maxdepth', type=float,
 	help="known bathymetric depth at location")
 parser.add_argument('--paramspan', nargs='+', type=float, 
 	help="max,min of parameter")
-
+parser.add_argument('--multiparamspan', nargs='+', type=float, 
+	help="max,min of parameter")
+parser.add_argument('-alamo','--alamofloats', action="store_true",
+	help='work with alamo float data in sql database')
+parser.add_argument('-alamocycle','--alamofloats_cycle', type=int, nargs=2,
+	help='start and stop range for cycle number')
 args = parser.parse_args()
 
 #TODO: Duplicate code in xbt/axctd data can be combined to a more simpler routine
@@ -171,3 +178,110 @@ if args.xbt:
 	if args.save_excel:
 		writer = pd.ExcelWriter(args.filepath.split('.')[0] + '.xlsx')
 		xbtdata.to_excel(writer,sheet_name=args.filepath.split('/')[-1])
+
+#####
+# alamo floats
+#
+if args.alamofloats and (args.alamofloats_cycle[0] ==args.alamofloats_cycle[1] ):
+
+	startcycle=args.alamofloats_cycle[0]
+	endcycle=args.alamofloats_cycle[1]
+
+	#get information from local config file - a json formatted file
+	config_file = 'EcoFOCI_config/db_config/db_config_alamofloats.pyini'
+
+
+	EcoFOCI_db = EcoFOCI_db_ALAMO()
+	(db,cursor) = EcoFOCI_db.connect_to_DB(db_config_file=config_file)
+
+	#get db meta information for mooring
+	table = '9085'
+	Profile = EcoFOCI_db.read_profile(table=table, CycleNumber=startcycle, verbose=True)
+	EcoFOCI_db.close()
+
+	Pressure = np.array(sorted(Profile.keys()))
+	Temperature = np.array([Profile[x]['Temperature'] for x in sorted(Profile.keys()) ])
+
+	figscale = 3.
+
+	fig = plt.figure(1, figsize=(1, 3), facecolor='w', edgecolor='w')
+	ax1 = fig.add_subplot(111)
+	p1 = ax1.scatter(Temperature,Pressure,8,marker='.', edgecolors='none', c=Temperature, 
+        norm=MidpointNormalize(midpoint=0.),
+        vmin=args.paramspan[0], vmax=args.paramspan[1], 
+        cmap='seismic')
+
+	p1 = ax1.plot(np.zeros_like(Pressure),Pressure,'grey',linewidth=.15)
+	ax1.set_yticks(np.arange(0.,args.maxdepth + 25.,10.))
+
+	if args.maxdepth:
+		ax1.set_ylim([0,args.maxdepth])
+
+	if args.paramspan:
+		ax1.set_xlim([args.paramspan[0],args.paramspan[1]])
+
+	ax1.invert_yaxis()
+
+	fmt=mpl.ticker.ScalarFormatter(useOffset=False)
+	fmt.set_scientific(False)
+	ax1.xaxis.set_major_formatter(fmt)
+	ax1.tick_params(axis='both', which='major', bottom='off', top='off',labelbottom='off')
+	ax1.yaxis.set_ticklabels([])
+	plt.tight_layout()
+	plt.savefig(args.filepath + '.png', transparent=True, dpi = (150))
+	plt.close()
+
+if args.alamofloats and not (args.alamofloats_cycle[0] == args.alamofloats_cycle[1] ):
+
+	startcycle=args.alamofloats_cycle[0]
+	endcycle=args.alamofloats_cycle[1]
+
+	#get information from local config file - a json formatted file
+	config_file = 'EcoFOCI_config/db_config/db_config_alamofloats.pyini'
+
+
+	EcoFOCI_db = EcoFOCI_db_ALAMO()
+	(db,cursor) = EcoFOCI_db.connect_to_DB(db_config_file=config_file)
+
+	figscale = endcycle-startcycle
+
+	fig = plt.figure(1, figsize=(figscale, 3), facecolor='w', edgecolor='w')
+	ax1 = fig.add_subplot(111)
+	plt.hold(True)
+
+	for cycle in range(startcycle,endcycle+1,1):
+		offset = cycle - startcycle
+		#get db meta information for mooring
+		table = '9085'
+		Profile = EcoFOCI_db.read_profile(table=table, CycleNumber=cycle, verbose=True)
+
+		Pressure = np.array(sorted(Profile.keys()))
+		Temperature = np.array([Profile[x]['Temperature'] for x in sorted(Profile.keys()) ])
+
+
+		p1 = ax1.scatter(Temperature+offset,Pressure,30,marker='.', edgecolors='none', c=Temperature, 
+	        norm=MidpointNormalize(midpoint=1.5),
+	        vmin=args.paramspan[0], vmax=args.paramspan[1], 
+	        cmap='RdYlBu_r')#seismic or RdBu_r
+
+	#p1 = ax1.plot(np.zeros_like(Pressure),Pressure,'grey',linewidth=.15)
+	#ax1.set_yticks(np.arange(0.,args.maxdepth + 25.,10.))
+
+	if args.maxdepth:
+		ax1.set_ylim([0,args.maxdepth])
+
+	ax1.set_xlim([args.paramspan[0],args.paramspan[1]+figscale+1])
+
+	plt.colorbar(p1)
+	ax1.invert_yaxis()
+
+	fmt=mpl.ticker.ScalarFormatter(useOffset=False)
+	fmt.set_scientific(False)
+	ax1.xaxis.set_major_formatter(fmt)
+	ax1.tick_params(axis='both', which='major', bottom='off', top='off',labelbottom='off')
+	#ax1.yaxis.set_ticklabels([])
+	plt.tight_layout()
+	plt.savefig(args.filepath + '.png', transparent=False, dpi = (300))
+	plt.close()
+
+	EcoFOCI_db.close()
---
+++ io_utils/EcoFOCI_db_io.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+"""
+ Background:
+ --------
+ EcoFOCI_db_io.py
+ 
+ 
+ Purpose:
+ --------
+ Various Routines and Classes to interface with the mysql database that houses EcoFOCI meta data
+ 
+ History:
+ --------
+
+
+"""
+
+import pymysql
+import ConfigParserLocal 
+import datetime
+
+__author__   = 'Shaun Bell'
+__email__    = 'shaun.bell@noaa.gov'
+__created__  = datetime.datetime(2014, 01, 29)
+__modified__ = datetime.datetime(2016, 8, 10)
+__version__  = "0.1.0"
+__status__   = "Development"
+__keywords__ = 'netCDF','meta','header'
+
+class EcoFOCI_db_ALAMO(object):
+	"""Class definitions to access EcoFOCI Mooring Database"""
+
+	def connect_to_DB(self, db_config_file=None):
+		"""Try to establish database connection
+
+		Parameters
+		----------
+		db_config_file : str
+		    full path to json formatted database config file    
+
+		"""
+		self.db_config = ConfigParserLocal.get_config(db_config_file)
+		try:
+		    self.db = pymysql.connect(self.db_config['host'], 
+		    						  self.db_config['user'],
+		    						  self.db_config['password'], 
+		    						  self.db_config['database'], 
+		    						  self.db_config['port'])
+		except:
+		    print "db error"
+		    
+		# prepare a cursor object using cursor() method
+		self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
+		return(self.db,self.cursor)
+
+	def manual_connect_to_DB(self, host='localhost', user='viewer', 
+							 password=None, database='ecofoci', port=3306):
+		"""Try to establish database connection
+
+		Parameters
+		----------
+		host : str
+		    ip or domain name of host
+		user : str
+			account user
+		password : str
+			account password
+		database : str
+			database name to connect to
+		port : int
+			database port
+
+		"""	    
+		self.db_config['host'] = host
+		self.db_config['user'] = user
+		self.db_config['password'] = password
+		self.db_config['database'] = database
+		self.db_config['port'] = port
+
+		try:
+		    self.db = pymysql.connect(self.db_config['host'], 
+		    						  self.db_config['user'],
+		    						  self.db_config['password'], 
+		    						  self.db_config['database'], 
+		    						  self.db_config['port'])
+		except:
+		    print "db error"
+		    
+		# prepare a cursor object using cursor() method
+		self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
+		return(self.db,self.cursor)
+
+	def read_profile(self, table=None, CycleNumber=None, verbose=False):
+	    
+		sql = ("SELECT * from `{0}` WHERE `CycleNumber`= '{1}' ORDER BY `id` DESC ").format(table, CycleNumber)
+
+		if verbose:
+		    print sql
+
+		result_dic = {}
+		try:
+		    # Execute the SQL command
+		    self.cursor.execute(sql)
+		    # Get column names
+		    rowid = {}
+		    counter = 0
+		    for i in self.cursor.description:
+		        rowid[i[0]] = counter
+		        counter = counter +1 
+		    #print rowid
+		    # Fetch all the rows in a list of lists.
+		    results = self.cursor.fetchall()
+		    for row in results:
+		        result_dic[row['Pressure']] ={keys: row[keys] for val, keys in enumerate(row.keys())} 
+		    return (result_dic)
+		except:
+		    print "Error: unable to fecth data"
+
+
+	def close(self):
+		"""close database"""
+		self.db.close()
+
