dc9d15064b7ceab7e395ef30a50642b448e4ac1f
shaunwbell <shaun.w.bell@gmail.com>
2018-01-05 12:20:56 -0800

    add folium maps

--- Folium_Alamo_Argos_timeseries.py
+++ Folium_Alamo_Argos_timeseries.py
@@ -7,6 +7,7 @@
 import numpy as np
 import pandas as pd
 import urllib
+import datetime
 
 import cmocean
 import folium
@@ -34,7 +35,150 @@ def color_normalize(cmin,cmax,value):
     return (value-cmin)/(cmax-cmin)
 
 
-# In[66]:
+"""--------------------- Get ArgosData  ------------------------------------"""
+
+class ARGOS_SERVICE(object):
+    r"""Download and parse data from the ARGOS Archive"""
+
+    @staticmethod
+    def get_data(filename=None, ARGOS_Type=None):
+        r"""
+        Basic Method to open files.  Specific actions can be passes as kwargs for instruments
+        """
+
+        fobj = open(filename)
+        data = fobj.read()
+
+
+        buf = data
+        return BytesIO(buf.strip())
+
+    @staticmethod
+    def drifter_parse(fobj):
+        r"""
+
+        """
+        argo_to_datetime =lambda date: datetime.datetime.strptime(date, '%Y %j %H%M')
+
+        header=['argosid','lat','lon','year','doy','hhmm','s1','s2','s3','s4','s5','s6','s7','s8']
+        df = pd.read_csv(fobj,delimiter='\s+',header=0,
+          names=header,index_col=False,
+          dtype={'year':str,'doy':str,'hhmm':str,'s1':str,'s2':str,'s3':str,'s4':str,'s5':str,'s6':str,'s7':str,'s8':str},
+          parse_dates=[['year','doy','hhmm']],date_parser=argo_to_datetime)
+
+        df.set_index(pd.DatetimeIndex(df['year_doy_hhmm']),inplace=True)
+        df.drop('year_doy_hhmm',axis=1,inplace=True)
+
+        return df
+
+def sst_argos(s1,s2):
+    try:
+        output = int(format(int(s1,16),'08b')[6:] + format(int(s2,16),'08b'),2) 
+        output = (output * 0.04) - 2.0   
+    except:
+        output = 1e35
+    return output
+
+def strain_argos(s1,manufacter='MetOcean'):
+    try:
+      converted_word = int(format(int(s1,16),'08b'),2)
+      if manufacter == 'MetOcean':
+        output = converted_word
+      else:
+        output = converted_word / 100.
+    except:
+      output = 1e35
+    return output
+
+def voltage_argos(s1):
+    try:
+        converted_word = int(format(int(s1,16),'08b')[:6],2)
+        output = (converted_word * 0.2) + 5   
+    except:
+        output = 1e35
+    return output
+
+def checksum_argos(s1,s2,s3,s4):
+    try:
+        converted_word = int(format(int(s1,16),'08b'),2) + \
+                         int(format(int(s2,16),'08b'),2) + \
+                         int(format(int(s3,16),'08b'),2)
+        checksum_test = converted_word % 256 
+        if  checksum_test == int(format(int(s4,16),'08b'),2):
+          output = True
+        else:
+          output = False
+    except:
+        output = 1e35
+    return output
+
+## Data is read in from raw drifter id files
+argo_to_datetime =lambda date: datetime.datetime.strptime(date, '%Y %j %H%M')
+
+header=['argosid','lat','lon','year','doy','hhmm','s1','s2','s3','s4','s5','s6','s7','s8']
+df_122537 = pd.read_csv('data/drifters/122537.y2017',delimiter='\s+',header=0,
+  names=header,index_col=False,
+  dtype={'year':str,'doy':str,'hhmm':str,'s1':str,'s2':str,'s3':str,'s4':str,'s5':str,'s6':str,'s7':str,'s8':str},
+  parse_dates=[['year','doy','hhmm']],date_parser=argo_to_datetime,error_bad_lines=False)
+
+df_122537.set_index(pd.DatetimeIndex(df_122537['year_doy_hhmm']),inplace=True)
+df_122537.drop('year_doy_hhmm',axis=1,inplace=True)
+
+df_122537['doy'] = [x.to_pydatetime().timetuple().tm_yday for x in df_122537.index]
+df_122537['fracday'] = [((x.to_pydatetime().timetuple().tm_hour*60.) + x.to_pydatetime().timetuple().tm_min)/1440. for x in df_122537.index]
+df_122537['doyfrac'] = df_122537['doy'] + df_122537['fracday']
+
+##
+df_122541 = pd.read_csv('data/drifters/122541.y2017',delimiter='\s+',header=0,
+  names=header,index_col=False,
+  dtype={'year':str,'doy':str,'hhmm':str,'s1':str,'s2':str,'s3':str,'s4':str,'s5':str,'s6':str,'s7':str,'s8':str},
+  parse_dates=[['year','doy','hhmm']],date_parser=argo_to_datetime,error_bad_lines=False)
+
+df_122541.set_index(pd.DatetimeIndex(df_122541['year_doy_hhmm']),inplace=True)
+df_122541.drop('year_doy_hhmm',axis=1,inplace=True)
+
+df_122541['doy'] = [x.to_pydatetime().timetuple().tm_yday for x in df_122541.index]
+df_122541['fracday'] = [((x.to_pydatetime().timetuple().tm_hour*60.) + x.to_pydatetime().timetuple().tm_min)/1440. for x in df_122541.index]
+df_122541['doyfrac'] = df_122541['doy'] + df_122541['fracday']
+
+##
+df_122542 = pd.read_csv('data/drifters/122542.y2017',delimiter='\s+',header=0,
+  names=header,index_col=False,
+  dtype={'year':str,'doy':str,'hhmm':str,'s1':str,'s2':str,'s3':str,'s4':str,'s5':str,'s6':str,'s7':str,'s8':str},
+  parse_dates=[['year','doy','hhmm']],date_parser=argo_to_datetime,error_bad_lines=False)
+
+df_122542.set_index(pd.DatetimeIndex(df_122542['year_doy_hhmm']),inplace=True)
+df_122542.drop('year_doy_hhmm',axis=1,inplace=True)
+
+df_122542['doy'] = [x.to_pydatetime().timetuple().tm_yday for x in df_122542.index]
+df_122542['fracday'] = [((x.to_pydatetime().timetuple().tm_hour*60.) + x.to_pydatetime().timetuple().tm_min)/1440. for x in df_122542.index]
+df_122542['doyfrac'] = df_122542['doy'] + df_122542['fracday']
+
+##
+df_136868 = pd.read_csv('data/drifters/136868.y2017',delimiter='\s+',header=0,
+  names=header,index_col=False,
+  dtype={'year':str,'doy':str,'hhmm':str,'s1':str,'s2':str,'s3':str,'s4':str,'s5':str,'s6':str,'s7':str,'s8':str},
+  parse_dates=[['year','doy','hhmm']],date_parser=argo_to_datetime,error_bad_lines=False)
+
+df_136868.set_index(pd.DatetimeIndex(df_136868['year_doy_hhmm']),inplace=True)
+df_136868.drop('year_doy_hhmm',axis=1,inplace=True)
+
+df_136868['doy'] = [x.to_pydatetime().timetuple().tm_yday for x in df_136868.index]
+df_136868['fracday'] = [((x.to_pydatetime().timetuple().tm_hour*60.) + x.to_pydatetime().timetuple().tm_min)/1440. for x in df_136868.index]
+df_136868['doyfrac'] = df_136868['doy'] + df_136868['fracday']
+
+##
+df_136869 = pd.read_csv('data/drifters/136869.y2017',delimiter='\s+',header=0,
+  names=header,index_col=False,
+  dtype={'year':str,'doy':str,'hhmm':str,'s1':str,'s2':str,'s3':str,'s4':str,'s5':str,'s6':str,'s7':str,'s8':str},
+  parse_dates=[['year','doy','hhmm']],date_parser=argo_to_datetime,error_bad_lines=False)
+
+df_136869.set_index(pd.DatetimeIndex(df_136869['year_doy_hhmm']),inplace=True)
+df_136869.drop('year_doy_hhmm',axis=1,inplace=True)
+
+df_136869['doy'] = [x.to_pydatetime().timetuple().tm_yday for x in df_136869.index]
+df_136869['fracday'] = [((x.to_pydatetime().timetuple().tm_hour*60.) + x.to_pydatetime().timetuple().tm_min)/1440. for x in df_136869.index]
+df_136869['doyfrac'] = df_136869['doy'] + df_136869['fracday']
 
 
 """--------------------- Get Data  -----------------------------------------"""
@@ -161,7 +305,7 @@ tol = .00001
 # In[221]:
 browser = webdriver.Chrome()
 
-for doymax in range(255,270,1):
+for doymax in range(255,365,1):
     for hourmax in range(0,24,12):
         f = folium.map.FeatureGroup()
         
@@ -295,10 +439,106 @@ for doymax in range(255,270,1):
                     )
                     )
         
+        ### resample to fixed time grid and fill interpolated temperatures with large value
         
-        # In[222]:
+        df_122537_t = df_122537.resample('12H').mean()
+        df_122537_t=df_122537_t[df_122537_t.index >= datetime.datetime(2017,9,15)]
+        df_122537_t = df_122537_t[df_122537_t['doyfrac'] <= (doymax + hourmax/24.0)]
+        df_122537_t=df_122537_t.interpolate()
         
-        
+        f1a = folium.map.FeatureGroup()
+
+        if not df_122537_t.empty:
+            lats = df_122537_t['lat'].values
+            lngs = df_122537_t['lon'].values
+            
+            for lat, lng in zip(lats, lngs):
+                f1a.add_child(
+                    folium.features.CircleMarker(
+                        [lat, lng],
+                        radius=.1,
+                        color='#000000',
+                    )
+                    )        
+
+        df_122542_t = df_122542.resample('12H').mean()
+        df_122542_t=df_122542_t[df_122542_t.index >= datetime.datetime(2017,9,15)]
+        df_122542_t = df_122542_t[df_122542_t['doyfrac'] <= (doymax + hourmax/24.0)]
+        df_122542_t=df_122542_t.interpolate()
+
+        f2a = folium.map.FeatureGroup()
+
+        lats = df_122542_t['lat'].values
+        lngs = df_122542_t['lon'].values
+
+        for lat, lng in zip(lats, lngs):
+            f2a.add_child(
+                folium.features.CircleMarker(
+                    [lat, lng],
+                    radius=.1,
+                    color='#000000',
+                )
+                )
+
+        df_122541_t = df_122541.resample('12H').mean()
+        df_122541_t=df_122541_t[df_122541_t.index >= datetime.datetime(2017,9,15)]
+        df_122541_t = df_122541_t[df_122541_t['doyfrac'] <= (doymax + hourmax/24.0)]
+        df_122541_t=df_122541_t.interpolate()
+
+        f3a = folium.map.FeatureGroup()
+
+        lats = df_122541_t['lat'].values
+        lngs = df_122541_t['lon'].values
+
+        for lat, lng in zip(lats, lngs):
+            f3a.add_child(
+                folium.features.CircleMarker(
+                    [lat, lng],
+                    radius=.1,
+                    color='#000000',
+                )
+                )
+
+        df_136868_t = df_136868.resample('12H').mean()
+        df_136868_t=df_136868_t[df_136868_t.index >= datetime.datetime(2017,9,15)]
+        df_136868_t = df_136868_t[df_136868_t['doyfrac'] <= (doymax + hourmax/24.0)]
+        df_136868_t = df_136868_t.interpolate()
+
+        f4a = folium.map.FeatureGroup()
+
+        lats = df_136868_t['lat'].values
+        lngs = df_136868_t['lon'].values
+
+        for lat, lng in zip(lats, lngs):
+            f4a.add_child(
+                folium.features.CircleMarker(
+                    [lat, lng],
+                    radius=.1,
+                    color='#000000',
+                )
+                )        
+
+        df_136869_t = df_136869.resample('12H').mean()
+        df_136869_t=df_136869_t[df_136869_t.index >= datetime.datetime(2017,9,15)]
+        df_136869_t = df_136869_t[df_136869_t['doyfrac'] <= (doymax + hourmax/24.0)]
+        df_136869_t = df_136869_t.interpolate()
+
+        f5a = folium.map.FeatureGroup()
+
+        lats = df_136869_t['lat'].values
+        lngs = df_136869_t['lon'].values
+
+        for lat, lng in zip(lats, lngs):
+            f5a.add_child(
+                folium.features.CircleMarker(
+                    [lat, lng],
+                    radius=.1,
+                    color='#000000',
+                )
+                )
+
+
+        ##
         m = folium.Map(location=[71,-165],
                       tiles='https://api.mapbox.com/styles/v1/sbell/cj48p13bt265k2qvucuielhm7/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2JlbGwiLCJhIjoiY2lqbGlpaHBkMDAyanV5bHhqMTdjYTd5aiJ9.04PwNcY3Piny-YtIg5cIJA',
                       attr='North Star - MapBox',
@@ -306,10 +546,15 @@ for doymax in range(255,270,1):
                       png_enabled=True)
         m.add_child(f1)
         m.add_child(f1s)
+        m.add_child(f1a)
         m.add_child(f2)
         m.add_child(f2s)
+        m.add_child(f2a)
         m.add_child(f3)
         m.add_child(f3s)
+        m.add_child(f3a)
+        m.add_child(f4a)
+        m.add_child(f5a)
         
         # In[220]:
         
